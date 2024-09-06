2d3293dd837cf4612d2ec21b522722efb86355c0
M. J. Fromberger <michael.j.fromberger@gmail.com>
2024-09-06 14:13:08 -0700

    scheddle: exercise order more aggressively in tests

--- scheddle_test.go
+++ scheddle_test.go
@@ -55,11 +55,22 @@ func TestQueue_basic(t *testing.T) {
 			t.Errorf("Got value %d, want %d", got, want)
 		}
 	}
+	noVal := func(d time.Duration) {
+		select {
+		case p := <-vals:
+			t.Errorf("Unexpected value %d", p)
+		case <-time.After(d):
+			t.Log("no more values for now, OK")
+		}
+	}
 
 	// Schedule 5 tasks at tick intervals:
 	//    ^----^----^----^----^
 	//    1    2    3    4    5
-	for i := range 5 {
+	//
+	// Schedule them "out of order". Verify
+	// Verify that they are done in time order below.
+	for i := range []int{2, 0, 4, 1, 3} {
 		q.After(time.Duration(i)*tick, push(i+1))
 	}
 
@@ -75,22 +86,25 @@ func TestQueue_basic(t *testing.T) {
 	q.After(10*tick, push(10))
 
 	clk.Advance(longTick) // A
+	q.After(11*tick, push(11))
 	want(1)
 	want(2)
 
-	q.After(11*tick, push(11))
+	noVal(2 * tick)
 
 	clk.Advance(longTick) // B
 	want(3)
-
 	q.After(12*tick, push(12))
+	want(4)
+
+	noVal(2 * tick)
 
 	clk.Advance(longTick) // C
-	want(4)
 	want(5)
-
 	q.After(13*tick, push(13))
 	q.After(14*tick, push(14))
+
+	noVal(2 * tick)
 
 	// A lot of time passes, all the additional tasks should now be eligible and
 	// should run in schedule order.
